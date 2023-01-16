1c5fd1034c4e5b8bbd29819aacdd5454cf8bb62e
rohithgadu <gadurohith@gmail.com>
2023-01-16 23:05:34 +0530

    Initial Commit

--- pkg/routes/trademark-routes.go
+++ pkg/routes/trademark-routes.go
@@ -9,26 +9,25 @@ import (
 
 func GetRoutes() {
 	app := fiber.New()
-	app.Get("/", controllers.HandleError)
-	app.Get("api/number/:id", controllers.GetDataById)
-	app.Get("api/name/:name", controllers.GetDataByName)
-	app.Listen(":8080")
-
 	limiterConfig := limiter.Config{
 		Next: func(c *fiber.Ctx) bool {
 			return c.IP() == "127.0.0.1:8080"
 		},
-		Max:        5,
-		Expiration: 30 * time.Second,
+		Max:        10,
+		Expiration: 2 * time.Second,
 		//Storage:    myCustomStorage{},
 		KeyGenerator: func(c *fiber.Ctx) string {
 			return c.Get("x-forwarded-for")
 		},
 		LimitReached: func(c *fiber.Ctx) error {
-			return c.JSON("Limit reached")
+			return c.JSON("Request Limit has been Exceeded")
 		},
 	}
-
 	app.Use(limiter.New(limiterConfig))
+
+	app.Get("/", controllers.HandleError)
+	app.Get("api/number/:id", controllers.GetDataById)
+	app.Get("api/name/:name", controllers.GetDataByName)
+	app.Listen(":8080")
 
 }
